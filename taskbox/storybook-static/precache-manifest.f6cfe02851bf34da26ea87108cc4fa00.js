f422c01f56d13001ef365f1b6d05a64ba9c3d419
Gunish Matta <gunishmatta@gmail.com>
2020-07-15 21:12:31 +0530

    Deployed

---
+++ taskbox/storybook-static/precache-manifest.f6cfe02851bf34da26ea87108cc4fa00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6564d8013e1a188abdba6fbe61f03a74",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.d91cac70bbe8a303a886.bundle.js"
+  },
+  {
+    "url": "runtime~main.d91cac70bbe8a303a886.bundle.js"
+  },
+  {
+    "revision": "747618ef6ea64c1628d6",
+    "url": "static/css/main.75be51ad.chunk.css"
+  },
+  {
+    "url": "vendors~main.d91cac70bbe8a303a886.bundle.js"
+  },
+  {
+    "url": "vendors~main.d91cac70bbe8a303a886.bundle.js.LICENSE.txt"
+  }
+]);
